01103b323ca82173f5beeb20532c409f37434915
GisseMv <133522567+GisseMv@users.noreply.github.com>
2023-05-13 19:54:02 -0500

    FacturaController.java

--- src/main/java/com/example/demo/Controller/FacturaController.java
+++ src/main/java/com/example/demo/Controller/FacturaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.core.model.Model;
 import jakarta.validation.constraints.Null;
 
 
@@ -14,5 +15,40 @@ public class FacturaController {
     public String detalle(){
         return "detalle";
     }
+
+
+    @RequestMapping("/respuesta/{nombrep}/{cantidad}/{precio_U}/{cuponD}/{factura}")
+    public String calVentaProducto(
+      @PathVariable String nombre,
+      @PathVariable Integer cantidad,
+      @PathVariable Double precio_U,
+      @PathVariable String cuponD,
+      @PathVariable String factura,
+      Model model
+      ){
+      double Bruta= cantidad * precio_U;
+      double descuentoC;
+      int ultimoC = Integer.parseInt(cuponD.substring(cuponD.length() - 1));
+      if (ultimoC % 2 == 0){descuentoC = Bruta* 0.1;
+      }else{descuentoC = Bruta* 0.05;}
+      
+      double factura2;
+      if(factura.equals("si")){
+          factura2 = Bruta * 0.08;
+      }else{
+          factura2 = 0;
+      }
+      
+      double total = Bruta - descuentoC + factura2;
+  
+      model.addAttribute("nombre", nombre);
+      model.addAttribute("Bruta",Bruta);
+      model.addAttribute("descuentoC", descuentoC);
+      model.addAttribute("factura2", factura2);
+      model.addAttribute("total", total);
+  
+      return "respuesta";
 }
+
+
 
